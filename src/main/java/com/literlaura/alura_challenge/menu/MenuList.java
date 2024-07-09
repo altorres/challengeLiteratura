28dd723af4802f415f5b11a2eefed08eb2e63e5d
Alvaro Torres <altorres1292@gmail.com>
2024-07-08 22:57:05 -0500

    Subiendo challenge

---
+++ src/main/java/com/literlaura/alura_challenge/menu/MenuList.java
@@ -0,0 +1,29 @@
+package com.literlaura.alura_challenge.menu;
+
+public class MenuList {
+    private static String welcome = """
+            **************************************************
+                  BIENVENIDOS A LA BIBLIOTECA LITERALURA
+            **************************************************
+            """;
+    private static String menu = """
+                    **************************************************
+                                    MENU PRINCIPAL  
+                    **************************************************
+                        1. Buscar libros por título
+                        2. Mostrar todos los libros   
+                        3. Mostrar todos los autores
+                        4. Mostrar autores vivos en un año 
+                        5. Mostrar libros por idioma 
+                        0. Salir 
+                    **************************************************    
+                    """;
+
+    public static String showMenu() {
+        return menu;
+    }
+    public static String welcomeMsg(){
+        return welcome;
+    }
+
+}
